466288d35d0e6c035ed49b6f259db06d2f54cc08
Utkarsh <btoshine774@gmail.com>
2024-06-19 10:37:10 +0530

    added argparse

---
+++ argparse_1.py
@@ -0,0 +1,31 @@
+import argparse
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Simple calculator for basic arithmetic operations.')
+
+    # Adding positional arguments
+    parser.add_argument("number1", help='First number', type=int)
+    parser.add_argument("number2", help='Second number', type=int)
+    parser.add_argument("operation", help='Operation (add, subtract, multiply)')
+
+    args = parser.parse_args()
+
+    print(f'Number 1: {args.number1}')
+    print(f'Number 2: {args.number2}')
+    print(f'Operation: {args.operation}')
+
+    n1 = args.number1
+    n2 = args.number2
+    result = None
+
+    if args.operation == 'add':
+        result = n1 + n2
+    elif args.operation == 'subtract':
+        result = n1 - n2
+    elif args.operation == 'multiply':
+        result = n1 * n2
+    else:
+        print(f'Invalid operation: {args.operation}')
+        exit(1)
+
+    print(f'Result: {result}')
